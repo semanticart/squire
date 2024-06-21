c477efbb26c1aaa4568c771d2cf260639b2841f3
Jeffrey Chupp <jeff@semanticart.com>
2024-06-21 16:06:29 -0400

    Convert body markdown to HTML

--- pkg/converter/epub.go
+++ pkg/converter/epub.go
@@ -1,9 +1,13 @@
 package converter
 
 import (
+	"bytes"
 	"encoding/base64"
 
 	epub "github.com/go-shiori/go-epub"
+	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark/extension"
+	"github.com/yuin/goldmark/renderer/html"
 
 	"github.com/semanticart/squire/pkg/parser"
 
@@ -17,7 +21,29 @@ func cssContent() string {
 	return "data:text/plain;base64," + base64.StdEncoding.EncodeToString(epubCSS)
 }
 
+func newMarkdownConverter() goldmark.Markdown {
+	return goldmark.New(
+		goldmark.WithExtensions(extension.GFM),
+		goldmark.WithRendererOptions(
+			html.WithXHTML(),
+		),
+	)
+}
+
+func markdownToHTML(md goldmark.Markdown, markdown string) (string, error) {
+	var buf bytes.Buffer
+
+	err := md.Convert([]byte(markdown), &buf)
+
+	if err != nil {
+		return "", err
+	}
+
+	return buf.String(), nil
+}
+
 func ConvertToEPUB(story parser.Story) error {
+	md := newMarkdownConverter()
 	book, err := epub.NewEpub(story.Title)
 
 	if err != nil {
@@ -35,7 +61,13 @@ func ConvertToEPUB(story parser.Story) error {
 	for _, chapter := range story.Chapters {
 		body := "<h1>" + chapter.Title + "</h1>"
 
-		body += chapter.Body
+		mdBody, err := markdownToHTML(md, chapter.Body)
+
+		if err != nil {
+			return err
+		}
+
+		body += mdBody
 
 		if len(chapter.Choices) > 0 {
 			body += "<ul class=\"choices\">"
